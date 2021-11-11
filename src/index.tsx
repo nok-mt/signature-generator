eb47683c08b2ef546798b7530106f674a8a2d943
naoki <n.matsuoka24@gmail.com>
2021-11-11 15:30:15 +0900

    recommit

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { render } from "react-dom";
 
-const Main = <h1>Signature generator</h1>;
+const Main = <h1>署名ジェネレータ</h1>;
 
 render(Main, document.getElementById("app"));
